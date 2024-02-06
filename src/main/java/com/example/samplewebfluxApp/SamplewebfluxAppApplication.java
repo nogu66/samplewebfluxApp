42ceae1839c10912d5c5910676bc6be93c0c0ddf
Yuta <ezochi66@gmail.com>
2024-02-06 11:11:39 +0900

    first commit

---
+++ src/main/java/com/example/samplewebfluxApp/SamplewebfluxAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplewebfluxApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplewebfluxAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplewebfluxAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/samplewebfluxApp/SamplewebfluxAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplewebfluxApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplewebfluxAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
