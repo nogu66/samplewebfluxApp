538c9c67112c8d72cc31d54e2d723aea4f3d3d8c
Yuta <ezochi66@gmail.com>
2024-02-06 14:38:35 +0900

    リクエストハンドラの場合

--- src/main/java/com/example/samplewebfluxApp/SampleController.java
+++ src/main/java/com/example/samplewebfluxApp/SampleController.java
@@ -5,6 +5,7 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.*
 import static org.springframework.web.reactive.function.server.ServerResponse.*;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.reactive.function.server.RouterFunction;
@@ -19,7 +20,7 @@ public class SampleController {
     
     @Bean
     public RouterFunction<ServerResponse> routes() {
-        return route(GET("/f/hello"), this::hello).andRoute(GET("/f/hello2"), this::hello2);
+        return route(GET("/f/hello"), this::hello).andRoute(GET("/f/hello2"), this::hello2).andRoute(GET("/f/flux2"), this::flux2);
     }
 
     Mono<ServerResponse> hello(ServerRequest req) {
@@ -32,6 +33,10 @@ public class SampleController {
     
     @RequestMapping("/f/flux")
     Mono<Rendering> flux() {
-        return Mono.just(Rendering.view("flux").build());
+        return Mono.just(Rendering.view("flux").modelAttribute("title", "Flux/Request Handler").modelAttribute("msg", "これはリクエストハンドラのサンプルです。").build());
+    }
+
+    Mono<ServerResponse> flux2(ServerRequest req) {
+        return ok().contentType(MediaType.TEXT_HTML).render("flux");
     }
 }
